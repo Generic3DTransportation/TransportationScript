b24b272379c666a47a2f4885512a65d6900c56d6
mostrowski <mike.ski246@yahoo.com>
2018-01-04 13:05:39 +0100

    Update der GUI für die Animation

--- python/GUIAnimation.py
+++ python/GUIAnimation.py
@@ -12,100 +12,103 @@ import maya.cmds as cmds
 
 def UI():
 
-    #Loescht Fenster, wenn das Fenster davor schon offen war
+    # Loescht Fenster, wenn das Fenster davor schon offen war
     winID = "Animation"
     if cmds.window(winID, exists=True):
         cmds.deleteUI(winID)
 
-    #Erzeugt ein leeres Fenster
-    AnimationWindow = cmds.window("Animation", title = "Animation erzeugen", h = 300, w = 300, sizeable = False)
+    # Erzeugt ein leeres Fenster
+    animationWindow = cmds.window("Animation", title = "Animation erzeugen", h = 100, w = 400, sizeable = False)
 
-    #Layout erstellen
-    mainLayout = cmds.columnLayout(w = 400, h = 350)
+    # Layout erstellen
+    mainLayout = cmds.columnLayout(w = 400, h = 200)
+    cmds.separator(style='none', height=25)
 
-
-    #imagePath = cmds.internalVar(upd = True) + "icons/logo.png"
-    #cmds.image(w = 1, h = 1, image = imagePath)
-
-
-    #Table fuer die Motoren
-    row1mot = cmds.rowColumnLayout( numberOfColumns=6, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,205) ] )
+    # Rows fuer die einzelnen Motoren
+    row1mot = cmds.rowColumnLayout( numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180) ] )
 
     cmds.separator(style='none', width=50, height=15)
     cmds.text(label="Motor 1")
     cmds.separator(style = 'none', width = 10)
     motor1 = cmds.intField(minValue = 0, maxValue = 180)
     cmds.separator(style='none', width=10)
+    cmds.text(label="Grad")
     cmds.text(label="(0 - 180 Grad)")
     cmds.separator(style='none', height=25)
     cmds.setParent('..')
 
-    row2mot = cmds.rowColumnLayout(numberOfColumns=6, columnWidth=[(1,25),(2,100),(3,10),(4,50),(5,10),(6,205)])
+    row2mot = cmds.rowColumnLayout(numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180)])
 
     cmds.separator(style='none', width=50)
-    cmds.text(label="Motor 1")
+    cmds.text(label="Motor 2")
     cmds.separator(style='none', width=10)
     motor2 = cmds.intField(minValue=0, maxValue=180)
     cmds.separator(style='none', width=10)
+    cmds.text(label="Grad")
     cmds.text(label="(0 - 180 Grad)")
     cmds.separator(style='none', height=25)
     cmds.setParent('..')
 
-    row3mot = cmds.rowColumnLayout(numberOfColumns=6, columnWidth=[(1,25),(2,100),(3,10),(4,50),(5,10),(6,205)])
+    row3mot = cmds.rowColumnLayout(numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180)])
 
     cmds.separator(style='none', width=50)
-    cmds.text(label="Motor 1")
+    cmds.text(label="Motor 3")
     cmds.separator(style='none', width=10)
     motor3 = cmds.intField(minValue=0, maxValue=180)
     cmds.separator(style='none', width=10)
+    cmds.text(label="Grad")
     cmds.text(label="(0 - 180 Grad)")
     cmds.separator(style='none', height=25)
     cmds.setParent('..')
 
-    row4mot = cmds.rowColumnLayout(numberOfColumns=6, columnWidth=[(1,25),(2,100),(3,10),(4,50),(5,10),(6,205)])
+    row4mot = cmds.rowColumnLayout(numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180)])
 
     cmds.separator(style='none', width=50)
-    cmds.text(label="Motor 1")
+    cmds.text(label="Motor 4")
     cmds.separator(style='none', width=10)
     motor4 = cmds.intField(minValue=0, maxValue=180)
     cmds.separator(style='none', width=10)
+    cmds.text(label="Grad")
     cmds.text(label="(0 - 180 Grad)")
     cmds.separator(style='none', height=25)
     cmds.setParent('..')
 
-    row5mot = cmds.rowColumnLayout(numberOfColumns=6, columnWidth=[(1,25),(2,100),(3,10),(4,50),(5,10),(6,205)])
+    row5mot = cmds.rowColumnLayout(numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180)])
 
     cmds.separator(style='none', width=50)
-    cmds.text(label="Motor 1")
+    cmds.text(label="Motor 5")
     cmds.separator(style='none', width=10)
     motor5 = cmds.intField(minValue=0, maxValue=180)
     cmds.separator(style='none', width=10)
+    cmds.text(label="Grad")
     cmds.text(label="(0 - 180 Grad)")
     cmds.separator(style='none', height=25)
     cmds.setParent('..')
 
     cmds.setParent('..')
 
-    table2 = cmds.rowColumnLayout( numberOfColumns=7, columnWidth=[ (1,50),(2,100),(3,10),(4,25),(5,10),(6,100) ] )
+    # Ein Layout für die Angabe der Sekunden zwischen den Keyframes
+    table2 = cmds.rowColumnLayout( numberOfColumns=7, columnWidth=[ (1,25),(2,100),(3,10),(4,50),(5,10),(6,25),(7,180) ] )
 
     cmds.separator(style='none', width=50)
     cmds.text(label="Animationsabstand")
     cmds.separator(style='none', width=10)
-    abstand = cmds.intField(minValue=0, maxValue=25)
+    motor1 = cmds.intField(minValue=0, maxValue=25)
     cmds.separator(style='none', width=10)
-    cmds.text(label="Sekunden (0 - 25)")
-
+    cmds.text(label="sec")
+    cmds.text(label="(0 - 25 Sekunden)")
     cmds.separator(style='none', height=25)
 
     cmds.setParent('..')
 
+    # Layout für den Button
     table3 = cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[ (1,100),(2,200),(3,100) ] )
 
-    cmds.separator(style='none', width=100, height=15)
+    cmds.separator(style='none', width=100, height=25)
     cmds.button(label = 'Animationspunkt setzen', command = '')
     cmds.separator(style='none', width=100)
 
     cmds.setParent('..')
 
-    #Zeigt das leere Fentser an
+    # Zeigt das Fentser an
     cmds.showWindow(AnimationWindow)
